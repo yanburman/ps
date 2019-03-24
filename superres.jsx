dd2510e3e3bf0d620206f1148ce56b8499d5f91b
Yan Burman <yanburman@users.noreply.github.com>
2019-03-24 22:23:38 +0200

    Superres WIP

---
+++ superres.jsx
@@ -0,0 +1,23 @@
+#target photoshop
+
+var SuperResResizeAndAlign = function () {
+	var docRef = app.activeDocument;
+
+	docRef.resizeImage(
+	  UnitValue(200, '%'),
+	  UnitValue(200, '%'),
+	  null,
+	  ResampleMethod.NEARESTNEIGHBOR
+	);
+
+	var numLayers = docRef.artLayers.length;
+        var curLayer;
+
+	for (var i = numLayers - 1; i >= 0; --i) {
+		curLayer = docRef.artLayers[i];
+		//$.writeln(i + " : " +curLayer .name);
+		curLayer.selected = true;
+        }
+}
+
+SuperResResizeAndAlign()
